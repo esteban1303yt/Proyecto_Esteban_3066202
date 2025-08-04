cb86134e474123da3813a50ba5ff5f6d7b4f6e8f
esteban1303yt <esteban.garcia.valencia13@gmail.com>
2025-08-04 11:39:10 -0500

    primer commit

---
+++ front/index.js
@@ -0,0 +1,94 @@
+const API_URL = "https://localhost:3000/api/equipos";
+
+//Métodos CRUD para interactuar con la API de equipos
+async function obtenerEquipos() {
+    const res = await fetch(API_URL);
+    const equipos = await res.json();
+    return equipos;
+}
+
+async function crearEquipo(data) {
+    const res = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return await res.json();
+}
+
+async function actualizarEquipo(id, data) {
+    const res = await fetch(`${API_URL}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return await res.json();
+}
+
+async function eliminarEquipo(id, data) {
+    const res = await fetch(`${API_URL}/${id}`, {
+        method: 'DELETE'
+    });
+    return await res.json();
+}
+
+//Referencias a los elementos del DOM
+const contenedorCards = document.getElementById('contenedorCards');
+const templateCard = document.getElementById('templateCard');
+const datoForm = document.getElementById('datoForm');
+const nombre = document.getElementById('nombre');
+const btnCancelar = document.getElementById('btnCancelar');
+
+//Mostrar equipos al cargar la página en el template
+async function mostrarEquipos() {
+    contenedorCards.innerHTML = '';
+    const equipos = await obtenerEquipos();
+    equipos.forEach(equipo => {
+        const clone = templateCard.contentEditable.cloneNode(true);
+        clone.querySelector('.nombreEquipos').textContent = equipo.nombre_equipo;
+        clone.querySelector('.btn-editar').onclick = () => cargarEquipoParaEditar(equipo);
+        clone.querySelector('.btn-eliminar').onclick = () => eliminarEquipoHandler(equipo.id_equipo);
+        contenedorCards.appendChild(clone);
+    });
+}
+
+//Guardar o actualizar equipos
+datoForm.onsubmit = async (e) => {
+    e.preventDefault();
+    const data = { nombre_equipo: nombre.value };
+    if (id_equipo.value) {
+        await actualizarEquipo(id_equipo.value, data);
+    } else {
+        await crearEquipo(data);
+    }
+    datoForm.reset();
+    id_equipo.value = '';
+    await mostrarEquipos();
+};
+
+// Cancelar edición
+btnCancelar.onclick = () => {
+    datoForm.reset();
+    id_equipo.value = '';
+};
+
+// Cargar equipo para editar
+function cargarEquipoParaEditar(equipo) {
+    id_equipo.value = equipo.id_equipo;
+    nombre.value = equipo.nombre_equipo;
+}
+
+// Eliminar equipo
+async function eliminarEquipoHandler(id) {
+    if (confirm("¿Estás seguro de eliminar este equipo?")) {
+        await eliminarEquipo(id);
+        mostrarEquipos();
+    }
+}
+
+// Mostrar al cargar
+mostrarEquipos();
