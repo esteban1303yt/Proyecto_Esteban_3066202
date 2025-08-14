7ece91ca49f33686825d9d00f372e7182a16f4ee
Esteban <esteban.garcia.valencia13@email.com>
2025-08-14 11:27:10 -0500

    14 de agosto

--- back/src/db/DB.sql
+++ back/src/db/DB.sql
@@ -1,7 +1,21 @@
+-- DROP DATABASE futbol_equipos;
+
 CREATE DATABASE IF NOT EXISTS futbol_equipos;
 USE futbol_equipos;
 
 CREATE TABLE equipos(
     id_equipo INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL;
+    nombre_equipo VARCHAR(100)
 );
+
+-- Insert = POST
+INSERT INTO equipos (nombre_equipo)
+VALUES
+("Alemania"),
+("Holanda"),
+("Suecia"),
+("Francia"),
+("Noruega");
+
+-- Select = GET
+SELECT * FROM equipos;
